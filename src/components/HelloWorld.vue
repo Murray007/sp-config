032ff62523c2f4525b3a1b37a40fd74dbfbd92b1
xu hu <huxu@xudeMacBook-Pro.local>
2022-09-04 11:08:47 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <!--
- * @Description: 
- * @Date: 2022-06-17 15:31:16
- * @LastEditTime: 2022-06-17 15:45:51
+ * @Description: mytest
+ * @Date: 2022-06-17y15:31:16
+ * @LastEditTime: 2022-09-04 11:08:27
 -->
 <template>
   <testPinia />
 </template>
 <script lang="ts" setup>
-  import testPinia from '../components/testPinia.vue'
+import testPinia from '../components/testPinia.vue'
 </script>
 <style scoped></style>
